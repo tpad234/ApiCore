a08f9e02bf6bd65c59ec5ced55c2568aafdcc1ad
tim diender <tim.diender@student.fontys.nl>
2023-03-15 14:13:21 +0100

    1e test orm

---
+++ Core/models/Database.cs
@@ -0,0 +1,24 @@
+﻿using Core.DTO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    namespace Core.models
+    {
+        public class DatabaseContext : DbContext
+        {
+            public DbSet<GebruikerDTO> gebruikers { get; set; }
+
+            public DbSet<ItemDTO> items { get; set; }
+
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlite("Data Source=(LocalDb)\\MSSQLLocalDB");
+            }
+        }
+    }
